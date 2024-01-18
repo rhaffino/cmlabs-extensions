a3a31f4635c497d260d040ae3d9c7e30a122d864
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-01-18 10:32:33 +0700

    feat: fetch request to api

---
+++ tools/robottxt-checker/service_worker/utils/postToAnalyze.js
@@ -0,0 +1,26 @@
+const postToAnalyze = async (baseUrl) => {
+  const url =
+    "https://tools-api-w3m734lsga-as.a.run.app/api/robot-checker/check";
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ url: baseUrl }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error in postToAnalyze:", error);
+    return null;
+  }
+};
+
+export default postToAnalyze;
