51028330ca434ba813ad84174b8abe5a9798f139
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-01-19 14:46:17 +0700

    modif: adjust content sctipt, add auto submit and auto get url

--- tools/link-analyze/views/index/index.js
+++ tools/link-analyze/views/index/index.js
@@ -4,24 +4,34 @@ const loadingElement = document.getElementById("loading");
 // result
 const resultElement = document.getElementById("result");
 
-document.getElementById("url-form").addEventListener("submit", (event) => {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  tabChrome().then((currentUrl) => {
+    var urlContainer = document.getElementById("url-input");
+    urlContainer.value = currentUrl;
+  });
 
-  loadingElement.style.display = "block";
-  resultElement.innerHTML = "";
-
-  const url = document.getElementById("url-input").value;
-  console.log(url);
-
-  const message = {
-    event: "OnStartLinkAnalysis",
-    data: {
-      url,
-    },
-  };
-
-  chrome.runtime.sendMessage(message);
+  var logButton = document.getElementById("submit-btn");
+  logButton.addEventListener("click", function () {
+    launch();
+  });
 });
+
+const launch = () => {
+  showLoading(true);
+
+  tabChrome().then((currentUrl) => {
+    console.log(currentUrl);
+
+    const message = {
+      event: "OnStartLinkAnalysis",
+      data: {
+        url: currentUrl,
+      },
+    };
+
+    chrome.runtime.sendMessage(message);
+  });
+};
 
 chrome.runtime.onMessage.addListener((message) => {
   const { event, response, status, info } = message;
@@ -31,7 +41,7 @@ chrome.runtime.onMessage.addListener((message) => {
       if (status) {
         displayResultLinkAnalysis(response);
       } else {
-        loadingElement.style.display = "none";
+        showLoading(false);
         resultElement.innerHTML = "";
 
         const noValidUrlParagraph = document.createElement("p");
@@ -44,19 +54,69 @@ chrome.runtime.onMessage.addListener((message) => {
   }
 });
 
+function tabChrome() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var currentTab = tabs[0];
+      var currentUrl = currentTab.url;
+
+      resolve(currentUrl);
+    });
+  });
+}
+
+const showLoading = (status) => {
+  if (status) {
+    loadingElement.style.display = "block";
+  } else {
+    loadingElement.style.display = "none";
+  }
+};
+
 const displayResultLinkAnalysis = (response) => {
   console.log(response);
 
-  loadingElement.style.display = "none";
+  showLoading(false);
   resultElement.innerHTML = "";
 
   const internalLinks = response.data.internal_links.links;
   const externalLinks = response.data.external_links.links;
+  const dofollowLinks = response.data.dofollow_links.value;
+  const nofollowLinks = response.data.nofollow_links.value;
+  const totalLinks = response.data.links.value;
+
+  // total
+  const totalLinksParagraph = document.createElement("p");
+  totalLinksParagraph.textContent = `Total links: ${totalLinks}`;
+  resultElement.appendChild(totalLinksParagraph);
+
+  // dofollow
+  const dofollowLinksParagraph = document.createElement("p");
+  dofollowLinksParagraph.textContent = `Dofollow Links: ${dofollowLinks}`;
+  resultElement.appendChild(dofollowLinksParagraph);
+
+  // nofollow
+  const nofollowLinksParagraph = document.createElement("p");
+  nofollowLinksParagraph.textContent = `Nofollow Links: ${nofollowLinks}`;
+  resultElement.appendChild(nofollowLinksParagraph);
 
   // internal
   const internalLinksParagraph = document.createElement("p");
-  internalLinksParagraph.textContent = "Internal Links:";
+  internalLinksParagraph.textContent = `Internal Links: ${internalLinks.length}`;
   resultElement.appendChild(internalLinksParagraph);
+
+  // external
+  const externalLinksParagraph = document.createElement("p");
+  externalLinksParagraph.textContent = `External Links: ${externalLinks.length}`;
+  resultElement.appendChild(externalLinksParagraph);
+
+  const hr = document.createElement("hr");
+  resultElement.appendChild(hr);
+
+  // internal
+  const internalLinksParagraph2 = document.createElement("p");
+  internalLinksParagraph2.textContent = `Internal Link:`;
+  resultElement.appendChild(internalLinksParagraph2);
 
   const internalList = document.createElement("ul");
   internalList.classList.add("internal-links");
@@ -69,9 +129,9 @@ const displayResultLinkAnalysis = (response) => {
   });
 
   // external
-  const externalLinksParagraph = document.createElement("p");
-  externalLinksParagraph.textContent = "External Links:";
-  resultElement.appendChild(externalLinksParagraph);
+  const externalLinksParagraph2 = document.createElement("p");
+  externalLinksParagraph2.textContent = `External Links:`;
+  resultElement.appendChild(externalLinksParagraph2);
 
   const externalList = document.createElement("ul");
   externalList.classList.add("external-links");
@@ -85,18 +145,16 @@ const displayResultLinkAnalysis = (response) => {
 };
 
 const checkLocalStorage = () => {
-  loadingElement.style.display = "block";
+  showLoading(true);
   resultElement.innerHTML = "";
 
   chrome.storage.local.get(["response"], (result) => {
-    loadingElement.style.display = "none";
+    showLoading(false);
 
     if (result.response) {
       displayResultLinkAnalysis(result.response);
     } else {
-      const noHistoryParagraph = document.createElement("p");
-      noHistoryParagraph.textContent = "No histories";
-      resultElement.appendChild(noHistoryParagraph);
+      launch();
     }
   });
 };
