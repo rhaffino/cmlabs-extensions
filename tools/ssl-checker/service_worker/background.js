9a5c943894855ea3f8de84078104bbe0c7872a85
triabagus <triatop9@gmail.com>
2024-02-12 11:51:23 +0700

    [SSL Checker] Fix: limit alert

--- tools/ssl-checker/service_worker/background.js
+++ tools/ssl-checker/service_worker/background.js
@@ -1,9 +1,43 @@
 import postSSLChecker from "./utils/postSSLChecker.js";
 import validateAndExtractBaseUrl from "./utils/validateUrl.js";
 
+let tempCount;
+let tempLastFetchTime;
+
 chrome.runtime.onInstalled.addListener((details) => {
-  chrome.storage.local.clear(() => {
-    console.log("Fresh like new!");
+  // ambil data sebelumnya
+  chrome.storage.local.get(["count", "lastFetchTime"], (result) => {
+    console.log("GET LAST DATA");
+    if (result.count) {
+      tempCount = result.count;
+    }
+    if (result.lastFetchTime) {
+      tempLastFetchTime = new Date(result.lastFetchTime);
+    }
+
+    // clear storage setelah data sebelumnya diambil
+    chrome.storage.local.clear(() => {
+      console.log("Fresh like new!");
+
+      // jika ga ada, set init
+      if (!tempCount) {
+        tempCount = 0;
+      }
+      if (!tempLastFetchTime) {
+        tempLastFetchTime = null;
+      }
+
+      chrome.storage.local.set(
+        {
+          isDataFetched: false,
+          count: tempCount,
+          lastFetchTime: tempLastFetchTime.toString(),
+        },
+        () => {
+          console.log("isDataFetched and fetch count data init");
+        }
+      );
+    });
   });
 });
 
@@ -12,60 +46,131 @@ chrome.runtime.onMessage.addListener((message) => {
 
   switch (event) {
     case "OnStartLinkAnalysis":
+      console.log("OnStartLinkAnalysis");
       chrome.storage.local.set({
         response: null,
       });
       processAnalyze(data.url);
-      console.log("OnStartLinkAnalysis");
+      break;
+    case "onResetResponse":
+      resetLocal();
       break;
     default:
-      console.log("Unknown event");
+      console.log("Unknown event", event);
   }
 });
 
 const processAnalyze = async (url) => {
-  const baseUrl = validateAndExtractBaseUrl(url);
-  let baseUrls = baseUrl.replace(/^(http(s)?|ftp):\/\//, "");
-  baseUrls = baseUrls.substr(baseUrls.length - 1) === "/" ? baseUrls.slice(0, -1) : baseUrls;
+  chrome.storage.local.get(["count", "lastFetchTime"]).then(async (result) => {
+    let count = result.count || 0;
+    let lastFetchTime = result.lastFetchTime
+      ? new Date(result.lastFetchTime)
+      : null;
 
-  if (baseUrl) {
-    console.log("Valid URL", baseUrl);
+    // Limit 5
+    const currentTime = new Date();
 
-    const data = await postSSLChecker(baseUrls);
+    if (count >= 5) {
+      const timeDifference = currentTime - lastFetchTime;
+      const timeDifferenceInHours = timeDifference / 1000 / 60 / 60;
+      if (timeDifferenceInHours < 1) {
+        console.log("Reach Limit :3");
+        const message = {
+          event: "OnFinishLinkAnalysis",
+          status: false,
+          info: "You have reached the usage limit of this tool",
+          data: null,
+        };
+        chrome.runtime.sendMessage(message, () => {
+          if (chrome.runtime.lastError) {
+          }
+        });
+        return;
+      } else {
+        count = 0;
+        lastFetchTime = currentTime.toString();
 
-    if (data) {
-      console.log("Success to post to analyze", JSON.stringify(data));
+        chrome.storage.local.set({
+          count: count,
+          lastFetchTime: lastFetchTime,
+        });
 
-      chrome.storage.local.set({
-        response: data,
+        console.log("Limit reset :3");
+      }
+    }
+
+    const baseUrl = validateAndExtractBaseUrl(url);
+    let baseUrls = baseUrl.replace(/^(http(s)?|ftp):\/\//, "");
+    baseUrls =
+      baseUrls.substr(baseUrls.length - 1) === "/"
+        ? baseUrls.slice(0, -1)
+        : baseUrls;
+
+    if (baseUrl) {
+      console.log("Valid URL", baseUrl);
+      
+      chrome.storage.local.set({ isDataFetched: true }, () => {
+        console.log("isDataFetched saved as true.");
       });
 
-      const message = {
-        event: "OnFinishLinkAnalysis",
-        status: true,
-        response: data,
-      };
-      chrome.runtime.sendMessage(message);
+      const data = await postSSLChecker(baseUrls);
+
+      if (data) {
+        console.log("Success to post to analyze", JSON.stringify(data));
+
+        // chrome.storage.local.set({
+        //   response: data,
+        // });
+
+        const message = {
+          event: "OnFinishLinkAnalysis",
+          status: true,
+          response: data,
+        };
+
+        count++;
+        lastFetchTime = currentTime.toString();
+
+        chrome.storage.local.set({
+          count: count,
+          lastFetchTime: lastFetchTime,
+        });
+
+        chrome.runtime.sendMessage(message, () => {
+          if (chrome.runtime.lastError) {
+            chrome.storage.local.set({ response: data });
+          }
+        });
+      } else {
+        console.log("Failed to post to analyze");
+
+        const message = {
+          event: "OnFinishLinkAnalysis",
+          status: false,
+          info: "Gosh, its failed, please try again!",
+          data: null,
+        };
+        chrome.runtime.sendMessage(message);
+      }
     } else {
-      console.log("Failed to post to analyze");
+      console.log("Invalid URL");
 
       const message = {
         event: "OnFinishLinkAnalysis",
         status: false,
-        info: "Gosh, its failed, please try again!",
+        info: "Oops, sorry not valid url mate!",
         data: null,
       };
       chrome.runtime.sendMessage(message);
     }
-  } else {
-    console.log("Invalid URL");
+  });
+};
 
-    const message = {
-      event: "OnFinishLinkAnalysis",
-      status: false,
-      info: "Oops, sorry not valid url mate!",
-      data: null,
-    };
-    chrome.runtime.sendMessage(message);
-  }
+const resetLocal = () => {
+  chrome.storage.local.set({ response: null }, () => {
+    console.log("response set to null.");
+    chrome.storage.local.set({ isDataFetched: false }, () => {
+      console.log("isDataFetched saved as false.");
+    });
+  });
 };
