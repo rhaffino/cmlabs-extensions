438493d75ea8fba66a950aca79c47a1f3f351f3e
triabagus <triatop9@gmail.com>
2024-05-16 11:34:33 +0700

    Adjustment: hide limit because there is no login

--- tools/hreflang-checker/service_worker/background.js
+++ tools/hreflang-checker/service_worker/background.js
@@ -24,7 +24,7 @@ chrome.runtime.onInstalled.addListener((details) => {
       chrome.storage.local.set(
         {
           isDataFetched: false,
-          count: tempCount,
+          // count: tempCount,
           lastFetchTime: tempLastFetchTime?.toString(),
           thirdUseTime: JSON.stringify(tempThirdUseTime),
         },
@@ -84,7 +84,7 @@ const processAnalyze = async (url) => {
         lastFetchTime = currentTime.toString();
 
         chrome.storage.local.set({
-          count: count,
+          // count: count,
           lastFetchTime: lastFetchTime,
         });
       }
@@ -106,11 +106,11 @@ const processAnalyze = async (url) => {
           response: data,
         };
 
-        count++;
+        // count++;
         lastFetchTime = currentTime.toString();
 
         chrome.storage.local.set({
-          count: count,
+          // count: count,
           lastFetchTime: lastFetchTime,
         });
 
